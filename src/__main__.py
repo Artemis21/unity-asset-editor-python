e5fa02c86bcd66a80911d87a06fb61d4c3495d5a
Artemis <hollyshort21@gmail.com>
2021-02-22 10:24:40 +0000

    Remove accidentally committed testing code

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,3 @@
 """Tool for editing Unity assets."""
 from .reader_writers import AssetFile
 from .streams import Reader, Writer
-
-
-ASSET_FILE = (
-    '/home/artemis/.local/share/Steam/steamapps/common/'
-    'The Battle of Polytopia/Polytopia_Data/sharedassets0.assets'
-)
-# ASSET_FILE = './test2.assets'
-
-stream_in = open(ASSET_FILE, 'rb')
-reader = Reader(stream_in)
-stream_out = open('./test.assets', 'wb')
-writer = Writer(stream_out)
-
-assets = reader.read(AssetFile)
-writer.write(assets)
